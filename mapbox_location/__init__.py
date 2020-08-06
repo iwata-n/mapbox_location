7e8651c72ea37bb3115ebe16fa3709cf57659d33
Naoki Iwata <naoki.a.iwata@dena.com>
2020-08-06 22:04:16 +0900

    first commit

---
+++ mapbox_location/__init__.py
@@ -0,0 +1,10 @@
+from .<ファイル> import *
+
+__copyright__    = 'Copyright (C) 2020 Naoki Iwata'
+__version__      = '1.0.0'
+__license__      = 'MIT'
+__author__       = 'Naoki Iwata'
+__author_email__ = 'a.tokai.wain@gmail.com'
+__url__          = 'https://github.com/iwata-n/mapbox_location'
+
+__all__ = ['公開モジュール', ...]
