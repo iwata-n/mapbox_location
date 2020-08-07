3489a93547dd603a0b9ed33185df7858a8fbc9c8
Naoki Iwata <naoki.a.iwata@dena.com>
2020-08-07 16:01:06 +0900

    version up

--- mapbox_location/__init__.py
+++ mapbox_location/__init__.py
@@ -1,7 +1,7 @@
 from .viz import *
 
 __copyright__    = 'Copyright (C) 2020 Naoki Iwata'
-__version__      = '1.0.2'
+__version__      = '1.0.3'
 __license__      = 'MIT'
 __author__       = 'Naoki Iwata'
 __author_email__ = 'a.tokai.wain@gmail.com'
